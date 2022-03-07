326e1609cf4f6b8dc8ee3f188875f4ab0a6f5db7
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-03-07 12:23:15 +0530

    create footer component

---
+++ src/components/footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="wallet_footer">Powered by Arcana</footer>
+</template>
+
+<script>
+export default {
+  name: "WalletFooter",
+};
+</script>
+
+<style>
+.wallet_footer {
+  text-align: center;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 15px;
+}
+</style>
