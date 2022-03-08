87018f95b9423815cc2172b8c00b17d8a36bd6c8
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-03-08 17:51:10 +0530

    create oauth cta component

---
+++ src/components/oauthLogin.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="wallet_signin-oauth-container">
+    <p class="wallet_signin-oauth-text">sign in with</p>
+    <img
+      class="wallet_signin-oauth-icon"
+      src="../assets/images/google_icon.png"
+    />
+    <img
+      class="wallet_signin-oauth-icon"
+      src="../assets/images/twitter_icon.png"
+    />
+    <img
+      class="wallet_signin-oauth-icon"
+      src="../assets/images/github_icon.png"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "oAuthLogin",
+};
+</script>
+
+<style>
+.wallet_signin-oauth-container {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 200px;
+}
+
+.wallet_signin-oauth-text {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 10px;
+  line-height: 13px;
+}
+
+.wallet_signin-oauth-icon {
+  width: 24px;
+  height: 24px;
+}
+</style>
