6d2dbb21b69823165c36e9ad0aebdcf0dd24fe95
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-03-08 17:51:32 +0530

    insert oauth component, apply style changes

--- src/pages/signIn.vue
+++ src/pages/signIn.vue
@@ -1,64 +1,95 @@
 <template>
   <div class="wallet_signin-container">
-    <p class="wallet_signin-title">Welcome</p>
-    <p class="wallet_signin-desc">
-      We’ll email you a magic link for a password-free sign in.
-    </p>
-    <div class="wallet_signin-input_container">
-      <label class="wallet_signin-input_label">Email</label>
-      <input
-        class="wallet_signin-input_field"
-        placeholder="someone@example.com"
-      />
+    <div class="wallet_signin-body">
+      <div class="wallet_signin_title-desc">
+        <p class="wallet_signin-title">Welcome</p>
+        <p class="wallet_signin-desc">
+          We’ll email you a magic link for a password-free sign in.
+        </p>
+      </div>
+      <div class="wallet_signin-input_container">
+        <label class="wallet_signin-input_label">Email</label>
+        <input
+          class="wallet_signin-input_field"
+          placeholder="someone@example.com"
+        />
+      </div>
+      <button class="wallet_signin-button">Send magic link</button>
     </div>
-    <button class="wallet_signin-button">Send magic link</button>
-    <p class="wallet_signin-signup-text">
-      New to Arcana? <button class="wallet_signin-signup-cta">Sign Up</button>
-    </p>
+    <div class="wallet_signin-footer">
+      <oAuthLogin />
+      <p class="wallet_signin-signup-text">
+        New to Arcana? <button class="wallet_signin-signup-cta">Sign Up</button>
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
+import oAuthLogin from "../components/oauthLogin.vue";
+
 export default {
   name: "SignIn",
+  components: {
+    oAuthLogin,
+  },
 };
 </script>
 
 <style>
 .wallet_signin-container {
+  height: 100%;
   padding: 20px 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.wallet_signin-body {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.wallet_signin-footer {
+  width: 100%;
+  height: 62px;
+  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.wallet_signin_title-desc {
+  height: 76px;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
 }
 
 .wallet_signin-title {
-  font-family: Sora;
-  font-style: normal;
   font-weight: 600;
   font-size: 20px;
-  line-height: 25px;
-
   text-align: center;
   color: #101010;
 }
 
 .wallet_signin-desc {
-  font-family: Sora;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 16px;
-  line-height: 18px;
+  font-size: 14px;
   text-align: center;
-
   color: #101010;
 }
 
 .wallet_signin-input_container {
   display: flex;
   flex-direction: column;
+  width: 100%;
 }
 
 .wallet_signin-input_label {
-  font-style: normal;
-  font-weight: normal;
   font-size: 14px;
   color: #101010;
 }
@@ -71,11 +102,7 @@ export default {
   box-shadow: inset -1px -1px 1px rgba(255, 255, 255, 0.7),
     inset 1px 1px 2px rgba(174, 174, 192, 0.2);
   border-radius: 10px;
-
-  font-family: Sora;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 20px;
+  font-size: 14px;
   color: #101010;
 }
 
@@ -87,21 +114,14 @@ export default {
   border-radius: 10px;
   border: none;
   outline: none;
-  margin-top: 28px;
-
-  font-style: normal;
   font-weight: 600;
   font-size: 14px;
-  line-height: 18px;
   text-transform: uppercase;
 }
 
 .wallet_signin-signup-text {
-  font-style: normal;
   font-weight: 600;
   font-size: 12px;
-  line-height: 150%;
-  text-align: center;
 }
 
 .wallet_signin-signup-cta {
