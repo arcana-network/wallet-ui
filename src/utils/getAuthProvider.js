d0e4e9a158cf93f907444ccd845eb0ed320ba931
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-03-22 13:36:56 +0530

    fix auth provider config

--- src/utils/getAuthProvider.js
+++ src/utils/getAuthProvider.js
@@ -2,9 +2,9 @@ import { AuthProvider } from "@arcana/auth";
 
 export function getAuthProvider(appId) {
   return new AuthProvider({
-    appID: appId,
-    redirectUri: `${window.location.origin}/login`,
-    network: "testnet",
+    appId: appId,
+    redirectUri: `${window.location.origin}/redirect`,
+    network: "dev",
     uxMode: "popup",
   });
 }
