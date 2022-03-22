d06708f3d0fae812943c298369508863510c63e5
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-03-22 13:03:39 +0530

    create login redirect page

---
+++ src/pages/loginRedirect.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { AuthProvider } from "@arcana/auth";
+
+AuthProvider.handleRedirectPage("*");
+</script>
+
+<template>
+  <div>Please wait...</div>
+</template>
+
+<style></style>
