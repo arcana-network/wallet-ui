5c0b07cbf0b67891149091bccd1bd87c9999bcee
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-03-28 13:25:43 +0530

    add store to app

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from "vue";
 import JsonViewer from "vue-json-viewer";
-import App from "./App.vue";
-import { router } from "./routes/index";
+import { createPinia } from "pinia";
+import App from "@/App.vue";
+import { router } from "@/routes/index";
 
 const walletApp = createApp(App);
 
-walletApp.use(JsonViewer).use(router);
+walletApp.use(JsonViewer).use(router).use(createPinia());
 
 walletApp.mount("#app");
