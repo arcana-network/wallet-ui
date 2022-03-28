cb9723e4cc7bafa0d395cfcf361a27f356ab81c3
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-03-28 14:33:26 +0530

    rename to useUserStore

--- src/store/user.js
+++ src/store/user.js
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia";
 
-export const userStore = defineStore("user", {
+export const useUserStore = defineStore("user", {
   state: () => ({
     isLoggedIn: false,
     info: {},
