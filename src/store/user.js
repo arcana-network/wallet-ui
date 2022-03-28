96ec7e69738007a4f71a1b330665fbe8fe2ff456
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-03-28 14:33:09 +0530

    rename to isLoggedin

--- src/store/user.js
+++ src/store/user.js
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 
 export const userStore = defineStore("user", {
   state: () => ({
-    isUserLoggedIn: false,
+    isLoggedIn: false,
     info: {},
     appId: null,
   }),
