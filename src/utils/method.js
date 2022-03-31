d3aa845af9ea70dc36f4b70d484b4d124c5ec481
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-03-31 19:19:39 +0530

    create method and action obj to render on signmessage

---
+++ src/utils/method.js
@@ -0,0 +1,8 @@
+export const methodAndAction = {
+  eth_sign: "Signature",
+  eth_accounts: "Decryption",
+  personal_sign: "Personal Signature",
+  eth_signTypedData_v4: "Signature",
+  eth_sendTransaction: "Send Transaction",
+  eth_signTransaction: "Transaction Signature",
+};
