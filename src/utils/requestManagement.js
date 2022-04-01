acadd1640e9017a31cb1193bd7826a10d52015c1
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-04-01 09:43:51 +0530

    impl logic to await permission status

--- src/utils/requestManagement.js
+++ src/utils/requestManagement.js
@@ -1,12 +1,41 @@
 // Todo: Find a better place for these functions
 
-export function getSendRequestFn(handleRequest, keeper, router) {
+export function getSendRequestFn(handleRequest, keeper, router, requestStore) {
   return function sendRequest(request) {
-    return handleRequest(request, keeper, router);
+    return handleRequest(request, keeper, router, requestStore);
   };
 }
 
-export async function handleRequest(request, keeper) {
-  const response = await keeper.handleRequest(request);
-  keeper.reply(request.method, response);
+function watchPermissionStatus(store) {
+  return new Promise((resolve) => {
+    store.$subscribe((mutation, state) => {
+      if (state.permissionStatus !== "none") {
+        resolve(state.permissionStatus);
+      }
+    });
+  });
+}
+
+export async function handleRequest(request, keeper, router, requestStore) {
+  const isPermissionRequired = keeper.isPermissionRequired(request.method);
+  requestStore.setRequest(request, isPermissionRequired);
+  if (isPermissionRequired) {
+    router.push({
+      name: "signMessage",
+      params: {
+        reqId: request.id,
+      },
+    });
+    await watchPermissionStatus(requestStore);
+  }
+  if (requestStore.allowRequest) {
+    const response = await keeper.handleRequest(request);
+    keeper.reply(request.method, response);
+  } else {
+    keeper.reply(request.method, {
+      error: "user_deny",
+      result: null,
+      id: request.id,
+    });
+  }
 }
