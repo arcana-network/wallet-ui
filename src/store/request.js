32843c7d644e2cc57c5df6ac4911ee3b1d72c9bd
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-04-01 08:36:44 +0530

    set request store

---
+++ src/store/request.js
@@ -0,0 +1,23 @@
+import { defineStore } from "pinia";
+
+export const useRequestStore = defineStore("request", {
+  state: () => ({
+    currentRequest: null,
+    pendingStatus: "none", // "none", "pending", "fulfilled"
+    isPermissionRequired: false,
+    permissionStatus: "none", // "none", "approved", "rejected"
+  }),
+  actions: {
+    setRequest(request, isPermissionRequired) {
+      this.currentRequest = request;
+      this.isPermissionRequired = isPermissionRequired;
+      this.pendingStatus = "pending";
+    },
+    approveRequest() {
+      this.permissionStatus = "approved";
+    },
+    rejectRequest() {
+      this.permissionStatus = "rejected";
+    },
+  },
+});
