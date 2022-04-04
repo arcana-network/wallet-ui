550e0ce0dd232200bc3fc5a97c2f577c7434168b
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-04-04 13:29:18 +0530

    register toast componrnt

--- src/main.js
+++ src/main.js
@@ -4,11 +4,30 @@ import { createPinia } from "pinia";
 import App from "@/App.vue";
 import { router } from "@/routes/index";
 import { Buffer } from "buffer";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
+const toastOptions = {
+  timeout: 5000,
+  closeOnClick: false,
+  pauseOnFocusLoss: true,
+  pauseOnHover: true,
+  draggable: false,
+  draggablePercent: 0.6,
+  showCloseButtonOnHover: false,
+  closeButton: false,
+  icon: true,
+  rtl: false,
+};
 
 window.Buffer = Buffer;
 
 const walletApp = createApp(App);
 
-walletApp.use(JsonViewer).use(router).use(createPinia());
+walletApp
+  .use(JsonViewer)
+  .use(router)
+  .use(Toast, toastOptions)
+  .use(createPinia());
 
 walletApp.mount("#app");
