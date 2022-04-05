4517108e4cf47f1a65bb1e00d0ed399de55611e7
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-04-05 13:54:28 +0530

    load stream module

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,7 @@ module.exports = defineConfig({
       fallback: {
         buffer: require.resolve("buffer/"),
         crypto: require.resolve("crypto-browserify"),
+        stream: require.resolve("stream-browserify"),
       },
     },
     plugins: [
