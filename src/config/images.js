5be44199852d8dfac9104843536d40ad1d6a3e11
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-04-05 13:53:15 +0530

    add greentick icon to images config

--- src/config/images.js
+++ src/config/images.js
@@ -7,6 +7,7 @@ const images = {
     copyIcon: require("@/assets/images/copy-dark-mode-icon.png"),
     infoIcon: require("@/assets/images/info-dark-mode-icon.png"),
     arrowIcon: require("@/assets/images/arrow-dark-mode-icon.png"),
+    greenTickIcon: require("@/assets/images/green-tick.png"),
   },
   light: {
     googleIcon: require("@/assets/images/google-icon.png"),
@@ -16,6 +17,7 @@ const images = {
     copyIcon: require("@/assets/images/copy-icon.png"),
     infoIcon: require("@/assets/images/info-icon.png"),
     arrowIcon: require("@/assets/images/arrow-icon.png"),
+    greenTickIcon: require("@/assets/images/green-tick.png"),
   },
 };
 
