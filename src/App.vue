0966542a7be52e92efaf5d3a22a474df7e01bbc4
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-04-25 00:23:23 +0530

    handle trigger login from parent app

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,35 @@
 <script setup>
 import { connectToParent } from 'penpal'
 import { toRefs, watch } from 'vue'
+import { useRouter } from 'vue-router'
 
 import WalletFooter from '@/components/AppFooter.vue'
 import { useAppStore } from '@/store/app'
 import { useUserStore } from '@/store/user'
+import { getAuthProvider } from '@/utils/getAuthProvider'
 
 const user = useUserStore()
 const app = useAppStore()
 const { theme } = toRefs(app)
+const router = useRouter()
 
 const connectionWithoutLogin = connectToParent({
   methods: {
     isLoggedIn: () => user.isLoggedIn,
+    triggerLogin: handleLoginRequest,
   },
 })
+
+async function handleLoginRequest(type) {
+  const authProvider = getAuthProvider(`${app.id}`)
+  try {
+    await user.handleLogin(authProvider, type)
+    router.push('/')
+  } catch (error) {
+    console.log(error)
+    user.$reset() // resets user store if login fails
+  }
+}
 
 async function getAppTheme() {
   const connectionInstance = await connectionWithoutLogin.promise
@@ -68,7 +83,7 @@ getAppTheme()
     inset 1px 1px 2px rgb(174 174 192 / 20%);
   --box-shadow-dark: inset -2px -2px 4px rgb(57 57 57 / 44%),
     inset 5px 5px 10px rgb(11 11 11 / 50%);
-  --debossed-light-color: #eeeeee;
+  --debossed-light-color: #eee;
   --debossed-dark-color: #161616;
   --debossed-box-shadow-light: inset -1px -1px 1px rgb(255 255 255 / 70%),
     inset 1px 1px 2px rgb(174 174 192 / 20%);
@@ -136,8 +151,8 @@ getAppTheme()
 }
 
 body {
-  font-family: Sora, sans-serif;
   height: 100vh;
+  font-family: Sora, sans-serif;
 }
 
 #app {
@@ -145,18 +160,34 @@ body {
 }
 
 button {
+  height: 40px;
+
   /* TODO: Brainstrom on managing outlines. https://github.com/arcana-network/wallet-ui/pull/1#discussion_r824371816 */
   cursor: pointer;
   background: transparent;
   border: none;
   outline: none;
-  height: 40px;
 }
 
 @media (max-width: 235px) {
   button {
     height: 30px;
   }
+}
+
+.wallet__container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  height: 100%;
+  padding: var(--p-400);
+  overflow: hidden;
+  color: var(--fg-color);
+  background: var(--container-bg-color);
+}
+
+.wallet__container > * + * {
+  margin-top: 10px;
 }
 
 .flow-container > *:not(:first-child) {
@@ -167,27 +198,12 @@ button {
   margin-top: var(--flow-space-element);
 }
 
-.wallet__container {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  height: 100%;
-  padding: var(--p-400);
-  color: var(--fg-color);
-  background: var(--container-bg-color);
-  overflow: hidden;
-}
-
-.wallet__container > * + * {
-  margin-top: 10px;
-}
-
 .wallet__body {
   flex: 1;
+  overflow: auto;
   background: var(--content-bg-color);
   border-radius: 10px;
   box-shadow: 4px 5px 4px rgb(0 0 0 / 25%);
-  overflow: auto;
 }
 
 .v-popper--theme-tooltip {
