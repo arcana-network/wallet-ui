e9384aaf3200acc1aefdbdcb603039c608fab013
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-04-25 11:47:54 +0530

    move Request and Response models to Connection

--- src/models/Connection.ts
+++ src/models/Connection.ts
@@ -1,10 +1,47 @@
-import type { RequestMethod } from '@/models/Request'
-import type { Response } from '@/models/Response'
 import type { ThemeConfig } from '@/models/Theme'
+
+type RequestMethod =
+  | 'eth_sign'
+  | 'personal_sign'
+  | 'eth_decrypt'
+  | 'eth_signTypedData_v4'
+  | 'eth_signTransaction'
+  | 'eth_sendTransaction'
+  | 'eth_accounts'
+  | 'eth_getEncryptionPublicKey'
+
+const PERMISSIONS: Record<RequestMethod, boolean> = Object.freeze({
+  eth_sign: true,
+  personal_sign: true,
+  eth_decrypt: true,
+  eth_signTypedData_v4: true,
+  eth_signTransaction: true,
+  eth_sendTransaction: true,
+  eth_accounts: false,
+  eth_getEncryptionPublicKey: false,
+})
+
+type Request = {
+  id: number
+  method: RequestMethod
+  params: string[]
+}
+
+function requirePermission(request: Request): boolean {
+  return PERMISSIONS[request.method]
+}
+
+type Response = {
+  id: number
+  result: unknown
+}
 
 type ParentConnectionApi = {
   getThemeConfig(): ThemeConfig
   onMethodResponse(method: RequestMethod, response: Response): void
+  sendPendingRequestCount(count: number): void
 }
 
-export type { ParentConnectionApi }
+export { requirePermission, PERMISSIONS }
+
+export type { ParentConnectionApi, Request, RequestMethod, Response }
--- src/models/Response.ts
+++
@@ -1,6 +0,0 @@
-type Response = {
-  id: number
-  result: unknown
-}
-
-export type { Response }
