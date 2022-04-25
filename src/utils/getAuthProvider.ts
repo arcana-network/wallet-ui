62061e1a7b6d99318136fc82864ac2817e7b4cae
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-04-25 13:47:59 +0530

    set network value type instead of the params

--- src/utils/getAuthProvider.ts
+++ src/utils/getAuthProvider.ts
@@ -1,12 +1,15 @@
 import { AuthProvider } from '@arcana/auth'
 import type { InitParams } from '@arcana/auth/types/types'
 
+const AUTH_NETWORK = process.env
+  .VUE_APP_ARCANA_AUTH_NETWORK as InitParams['network']
+
 export function getAuthProvider(appId): AuthProvider {
   return new AuthProvider({
     appId: appId,
     redirectUri: `${window.location.origin}/redirect`,
-    network: process.env.VUE_APP_ARCANA_AUTH_NETWORK,
+    network: AUTH_NETWORK,
     uxMode: 'popup',
     debug: false,
-  } as InitParams)
+  })
 }
