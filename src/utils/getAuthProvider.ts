abc89be9ed1bedda8cd37b348359abd317d04d41
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-05-08 22:56:20 +0530

    move redirect url to env file

--- src/utils/getAuthProvider.ts
+++ src/utils/getAuthProvider.ts
@@ -4,13 +4,15 @@ import type { InitParams } from '@arcana/auth/types/types'
 const AUTH_NETWORK = process.env
   .VUE_APP_ARCANA_AUTH_NETWORK as InitParams['network']
 
+const REDIRECT_URL = process.env.VUE_APP_WALLET_AUTH_REDIRECT_URL
+
 let authProvider: AuthProvider | null = null
 
 export async function getAuthProvider(appId): Promise<AuthProvider> {
   if (!authProvider) {
     authProvider = await AuthProvider.init({
       appId: appId,
-      redirectUri: `https://wallet-verify.netlify.app/verify/${appId}/`,
+      redirectUri: `${REDIRECT_URL}/${appId}/`,
       network: AUTH_NETWORK,
       flow: 'redirect',
       autoRedirect: false,
