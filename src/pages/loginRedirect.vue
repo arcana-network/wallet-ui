a09fddcfe2b9a710fcc164143dfaba022dad49d4
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-05-08 16:23:01 +0530

    onmount create auth instance to get userinfo
    and set it in session store

--- src/pages/loginRedirect.vue
+++ src/pages/loginRedirect.vue
@@ -1,31 +1,30 @@
 <script setup lang="ts">
-import { AuthProvider } from '@arcana/auth'
 import { connectToParent } from 'penpal'
+import { onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 
 import type { ParentConnectionApi } from '@/models/Connection'
+import { getAuthProvider } from '@/utils/getAuthProvider'
 
 const route = useRoute()
 const { appId } = route.params
 
-const authProvider = await AuthProvider.init({
-  appId: `${appId}`,
-  redirectUri: `${window.location.origin}/${appId}/redirect`,
-  network: 'dev',
-  flow: 'redirect',
-  debug: true,
-})
+onMounted(init)
 
-if (authProvider.isLoggedIn()) {
-  const info = authProvider.getUserInfo()
-  sessionStorage.setItem('info', JSON.stringify(info))
-  sessionStorage.setItem('isLoggedIn', JSON.stringify(true))
+async function init() {
+  const authProvider = await getAuthProvider(`${appId}`)
+
+  if (authProvider.isLoggedIn()) {
+    const info = authProvider.getUserInfo()
+    sessionStorage.setItem('userinfo', JSON.stringify(info))
+    sessionStorage.setItem('isLoggedIn', JSON.stringify(true))
+  }
+
+  const parentAppUrl = localStorage.getItem('parentAppUrl')
+  const connectionToParent = await connectToParent<ParentConnectionApi>({})
+    .promise
+  connectionToParent.redirect(parentAppUrl)
 }
-const parentAppUrl = localStorage.getItem('parentAppUrl')
-const connection = connectToParent<ParentConnectionApi>({})
-connection.promise.then((parent) => {
-  parent.redirect(parentAppUrl)
-})
 </script>
 
 <template>
