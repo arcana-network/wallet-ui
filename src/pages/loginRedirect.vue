88ef72e2d7319577305110c15efe16c575df9079
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-05-08 10:02:02 +0530

    create auth instance in redirect
    1. user the auth instance to heck if user loggedin and get userinfo
    2. redirect to parent app

--- src/pages/loginRedirect.vue
+++ src/pages/loginRedirect.vue
@@ -1,7 +1,31 @@
 <script setup lang="ts">
 import { AuthProvider } from '@arcana/auth'
+import { connectToParent } from 'penpal'
+import { useRoute } from 'vue-router'
 
-AuthProvider.handleRedirectPage('*')
+import type { ParentConnectionApi } from '@/models/Connection'
+
+const route = useRoute()
+const { appId } = route.params
+
+const authProvider = await AuthProvider.init({
+  appId: `${appId}`,
+  redirectUri: `${window.location.origin}/${appId}/redirect`,
+  network: 'dev',
+  flow: 'redirect',
+  debug: true,
+})
+
+if (authProvider.isLoggedIn()) {
+  const info = authProvider.getUserInfo()
+  sessionStorage.setItem('info', JSON.stringify(info))
+  sessionStorage.setItem('isLoggedIn', JSON.stringify(true))
+}
+const parentAppUrl = localStorage.getItem('parentAppUrl')
+const connection = connectToParent<ParentConnectionApi>({})
+connection.promise.then((parent) => {
+  parent.redirect(parentAppUrl)
+})
 </script>
 
 <template>
