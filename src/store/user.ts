10c50737d805b27cdfaf42ba76ab1a8f553e6559
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-05-09 11:46:45 +0530

    disabled ts-check for now
    - this is for loginwithOTP in return type

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 import { AuthProvider } from '@arcana/auth'
 import type { LoginType, UserInfo } from '@arcana/auth/types/types'
 import { defineStore } from 'pinia'
