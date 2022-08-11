2e2bbc631df254bb95dc05a4647e26c1365e3b67
Rohith <rohit.narahari@gmail.com>
2022-08-11 14:59:49 +0530

    removed string encodings

--- src/utils/advancedInfo.ts
+++ src/utils/advancedInfo.ts
@@ -1,22 +1,31 @@
+function isJson(str: string) {
+  try {
+    JSON.parse(str)
+  } catch (e) {
+    return false
+  }
+  return true
+}
+
 export const advancedInfo = (method: string, params: string | string[]) => {
   let data
-  if (method == 'eth_sign') {
+  if (method == 'eth_sign' && isJson(params[1])) {
     const jsonData = JSON.parse(params[1])
     if (jsonData.domain.name == 'Arcana Forwarder') {
       data = {
-        'Account Address': `${params[0]}`,
+        'Account Address': params[0],
         'Meta-transaction request': {
-          version: `${jsonData.domain.version}`,
-          chainID: `${jsonData.domain.chainId}`,
-          Forwarder_Address: `${jsonData.domain.verifyingContract}`,
+          version: jsonData.domain.version,
+          chainID: jsonData.domain.chainId,
+          Forwarder_Address: jsonData.domain.verifyingContract,
         },
         'Message Info': {
-          value: `${jsonData.message.value}`,
+          value: jsonData.message.value,
           gas: `${jsonData.message.gas} (will be paid by Arcana Forwarder)`,
-          nonce: `${jsonData.message.nonce}`,
-          from: `${jsonData.message.from}`,
-          to: `${jsonData.message.to}`,
-          data: `${jsonData.message.data}`,
+          nonce: jsonData.message.nonce,
+          from: jsonData.message.from,
+          to: jsonData.message.to,
+          data: jsonData.message.data,
         },
       }
     } else {
@@ -24,38 +33,38 @@ export const advancedInfo = (method: string, params: string | string[]) => {
     }
   } else if (method == 'personal_sign') {
     data = {
-      'Account Address': `${params[1]}`,
-      'Message to Sign': `${params[0]}`,
+      'Account Address': params[1],
+      'Message to Sign': params[0],
     }
-  } else if (method == 'eth_signTypedData_v4') {
+  } else if (method == 'eth_signTypedData_v4' && isJson(params[1])) {
     const jsonData = JSON.parse(params[1])
     if (jsonData.domain.name == 'Arcana Forwarder') {
       data = {
-        'Account Address': `${params[0]}`,
+        'Account Address': params[0],
         'Meta-transaction request': {
-          version: `${jsonData.domain.version}`,
-          chainID: `${jsonData.domain.chainId}`,
-          Forwarder_Address: `${jsonData.domain.verifyingContract}`,
+          version: jsonData.domain.version,
+          chainID: jsonData.domain.chainId,
+          Forwarder_Address: jsonData.domain.verifyingContract,
         },
         'Message Info': {
-          value: `${jsonData.message.value}`,
+          value: jsonData.message.value,
           gas: `${jsonData.message.gas} (will be paid by Arcana Forwarder)`,
-          nonce: `${jsonData.message.nonce}`,
-          from: `${jsonData.message.from}`,
-          to: `${jsonData.message.to}`,
-          data: `${jsonData.message.data}`,
+          nonce: jsonData.message.nonce,
+          from: jsonData.message.from,
+          to: jsonData.message.to,
+          data: jsonData.message.data,
         },
       }
     } else {
-      data = JSON.stringify(params[1])
+      data = params[1]
     }
   } else if (method == 'eth_decrypt') {
     data = {
-      'Account Address': `${params[1]}`,
-      'Cipher text': `${params[0]}`,
+      'Account Address': params[1],
+      'Cipher text': params[0],
     }
   } else {
-    data = params[0]
+    data = params
   }
 
   return data
