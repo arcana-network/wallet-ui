40f7c8dc31d9843bcd444a6e2101d2beebc66d27
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-08-21 17:06:24 +0530

    create modal container

---
+++ src/components/ModalFullScreen.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="modal__container flex justify-center items-center absolute h-screen w-screen top-0 left-0 z-50 p-4"
+  >
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.modal__container {
+  backdrop-filter: blur(32px);
+}
+</style>
