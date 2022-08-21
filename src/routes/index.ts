0960b8c75fa2e37f62c0178a95e4ef9578a0a5d1
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-08-21 16:13:12 +0530

    add profile screen to routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/pages/homePage.vue'
 import InitPage from '@/pages/initPage.vue'
 import LoginRedirect from '@/pages/loginRedirect.vue'
+import ProfileScreen from '@/pages/profileScreen.vue'
 import SignIn from '@/pages/signIn.vue'
 import SignMessage from '@/pages/signMessage.vue'
 
@@ -27,6 +28,11 @@ const routes = [
     name: 'signMessage',
     path: '/signMessage',
     component: SignMessage,
+  },
+  {
+    name: 'profileScreen',
+    path: '/profileScreen',
+    component: ProfileScreen,
   },
 ]
 
