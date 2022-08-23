e0517182dc454cf45261021675b62d7fc7c74f6d
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-08-23 14:41:38 +0530

    use env var to set debug prop in penpal

--- src/utils/createParentConnection.ts
+++ src/utils/createParentConnection.ts
@@ -8,14 +8,14 @@ import {
 function createParentConnection(methods) {
   return connectToParent<ParentConnectionApi>({
     methods,
-    debug: false,
+    debug: process.env.VUE_APP_ENABLE_PENPAL_DEBUG === 'true',
   })
 }
 
 function createInitParentConnection(methods) {
   return connectToParent<InitParentConnectionApi>({
     methods,
-    debug: false,
+    debug: process.env.VUE_APP_ENABLE_PENPAL_DEBUG === 'true',
   })
 }
 
