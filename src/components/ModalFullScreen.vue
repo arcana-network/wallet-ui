bfc34a24e8e05308a2925f9391d873dc3a38c00a
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-08-26 11:02:13 +0530

    replace with tailwind classes

--- src/components/ModalFullScreen.vue
+++ src/components/ModalFullScreen.vue
@@ -1,13 +1,7 @@
 <template>
   <div
-    class="modal__container flex justify-center items-center absolute h-screen w-screen top-0 left-0 z-50 p-4"
+    class="backdrop-blur-[32px] flex justify-center items-center absolute h-screen w-screen top-0 left-0 z-50 p-4"
   >
     <slot />
   </div>
 </template>
-
-<style scoped>
-.modal__container {
-  backdrop-filter: blur(32px);
-}
-</style>
