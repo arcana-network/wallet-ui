79a4196d34bd6973edd6a20040957c02455482f0
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-09-01 18:08:51 +0530

    rename to BaseModal and remove slot

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,7 +1,6 @@
 <template>
   <div
+    id="modal-container"
     class="backdrop-blur-[32px] flex justify-center items-center absolute h-screen w-screen top-0 left-0 z-50 p-4"
-  >
-    <slot />
-  </div>
+  ></div>
 </template>
