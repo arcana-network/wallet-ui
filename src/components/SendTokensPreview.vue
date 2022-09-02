4b05e4d8dbe828001a11aead08163d27ca4eb7ed
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-09-02 16:43:46 +0530

    create send tokens preview component

---
+++ src/components/SendTokensPreview.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import { useImage } from '@/utils/useImage'
+
+const getImage = useImage()
+
+const emits = defineEmits(['close, submit'])
+</script>
+
+<template>
+  <div
+    class="container space-y-4 sm:space-y-3 rounded-lg overflow-auto h-full min-w-full p-3 sm:p-2 flex flex-col"
+  >
+    <div class="flex flex-col space-y-3 sm:space-y-2">
+      <p class="text-xl sm:text-sm">Preview</p>
+    </div>
+    <div class="space-y-1">
+      <p class="text-xs text-zinc-400">Network</p>
+      <p class="text-base sm:text-sm">Ethereum</p>
+    </div>
+    <div class="space-y-1">
+      <p class="text-xs text-zinc-400">Sender’s Wallet Address</p>
+      <p class="text-base truncate">19emjx4vqHPn6ZTsh1ZNbBD7uFZFqWA5Cq</p>
+    </div>
+    <div class="flex justify-center">
+      <img :src="getImage('arrow-down-icon')" alt="arrow down icon" />
+    </div>
+    <div class="space-y-1">
+      <p class="text-xs text-zinc-400">Recipient’s Wallet Address</p>
+      <p class="text-sm truncate">19emjx4vqHPn6ZTsh1ZNbBD7uFZFqWA5Cq</p>
+    </div>
+    <div class="space-y-3">
+      <div class="flex justify-between text-xs text-zinc-400">
+        <p>Items</p>
+        <p>Amount</p>
+      </div>
+      <div class="space-y-[10px]">
+        <div class="flex justify-between text-sm sm:text-xs font-normal">
+          <p>Send Amount</p>
+          <p>1.2 ETH</p>
+        </div>
+        <div class="flex justify-between text-sm sm:text-xs font-normal">
+          <p>Gas Fees</p>
+          <p>.0300 ETH</p>
+        </div>
+      </div>
+      <div
+        class="flex justify-between text-sm sm:text-xs font-normal border-y-[1px] border-x-0 border-zinc-400 py-4"
+      >
+        <p>Total:</p>
+        <p>1.23 ETH</p>
+      </div>
+    </div>
+    <div class="flex justify-between">
+      <button
+        class="text-sm sm:text-xs rounded-xl border-2 border-black dark:border-white bg-transparent text-black dark:text-white w-36 h-9 sm:w-20 sm:h-8"
+        @click="emits('close')"
+      >
+        Back
+      </button>
+      <button
+        class="text-sm sm:text-xs rounded-xl text-white dark:bg-white bg-black dark:text-black w-36 h-9 sm:w-20 sm:h-8"
+        @click="emits('submit')"
+      >
+        Send
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  background: var(--content-bg-color);
+  box-shadow: 4px 5px 4px rgb(0 0 0 / 25%);
+}
+</style>
