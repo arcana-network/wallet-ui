fd981557933c8d2559fdb807de188d1140829069
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-09-06 19:20:20 +0530

    add send transactions to routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import HomePage from '@/pages/homePage.vue'
 import InitPage from '@/pages/initPage.vue'
 import LoginRedirect from '@/pages/loginRedirect.vue'
 import ProfileScreen from '@/pages/profileScreen.vue'
+import SendTransaction from '@/pages/sendTransaction.vue'
 import SignIn from '@/pages/signIn.vue'
 import SignMessage from '@/pages/signMessage.vue'
 
@@ -33,6 +34,11 @@ const routes = [
     name: 'profileScreen',
     path: '/profileScreen',
     component: ProfileScreen,
+  },
+  {
+    name: 'sendTransaction',
+    path: '/sendTransaction',
+    component: SendTransaction,
   },
 ]
 
