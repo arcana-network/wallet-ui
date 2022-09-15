5e50008ab6c0d27fb804753e374bfb50ff34c927
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-09-15 16:01:51 +0530

    convert wei to string

--- src/utils/gweiToEth.ts
+++ src/utils/gweiToEth.ts
@@ -1,7 +1,7 @@
 import { ethers } from 'ethers'
 
 function convertGweiToEth(value) {
-  const valueInWei = value * Math.pow(10, 9)
+  const valueInWei = `${value * Math.pow(10, 9)}` // converting to string to prevent overflow - https://docs.ethers.io/v5/troubleshooting/errors/#help-NUMERIC_FAULT-overflow
   return ethers.utils.formatEther(valueInWei)
 }
 
