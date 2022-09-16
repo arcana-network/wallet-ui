07319d5e8c33d526871aebc9311bdb9f8df7fc38
Karthik Durai <iamkarthiksiddarth@gmail.com>
2022-09-16 15:30:56 +0530

    [AR-4340] Replace placeholder text in wallet UI (#88)
    * remove unused imports
    
    * replace placeholder texts

--- src/components/SignMessage.vue
+++ src/components/SignMessage.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
-import { ref } from 'vue'
 
 import DateTime from '@/components/DateTime.vue'
 import SignMessageAdvancedInfo from '@/components/signMessageAdvancedInfo.vue'
