370d635689b139826bcf4123093b433ba3cce150
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-09-24 18:09:32 +0530

    create form to add network

---
+++ src/components/AddNetwork.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import { useImage } from '@/utils/useImage'
+
+const emits = defineEmits(['close'])
+
+const getImage = useImage()
+
+const networkName = ref('')
+</script>
+
+<template>
+  <div class="space-y-3">
+    <div class="flex justify-between">
+      <p class="text-xl sm:text-sm">Add a Network</p>
+      <button class="h-auto" @click="emits('close')">
+        <img :src="getImage('close-icon')" alt="close form" />
+      </button>
+    </div>
+    <form class="space-y-3 sm:space-y-2">
+      <div class="space-y-1">
+        <label class="text-xs text-zinc-400" for="networkName">
+          Network Name
+        </label>
+        <input
+          id="networkName"
+          v-model="networkName"
+          required
+          type="text"
+          class="text-base sm:text-sm bg-gradient w-full p-2 sm:p-1 rounded-lg border-none outline-none"
+          placeholder="e.g. Ethereum"
+        />
+      </div>
+      <div class="space-y-1">
+        <label class="text-xs text-zinc-400" for="rpcUrl"> RPC URL </label>
+        <input
+          id="rpcUrl"
+          v-model="networkName"
+          required
+          type="text"
+          class="text-base sm:text-sm bg-gradient w-full p-2 sm:p-1 rounded-lg border-none outline-none"
+          placeholder="e.g. https://blockchain.dev.arcana.network"
+        />
+      </div>
+      <div class="space-y-1">
+        <label class="text-xs text-zinc-400" for="currencySymbol">
+          Currency Symbol
+        </label>
+        <input
+          id="currencySymbol"
+          v-model="networkName"
+          required
+          type="text"
+          class="text-base sm:text-sm bg-gradient w-full p-2 sm:p-1 rounded-lg border-none outline-none"
+          placeholder="e.g. ETH"
+        />
+      </div>
+      <div class="space-y-1">
+        <label class="text-xs text-zinc-400" for="chainId"> Chain ID </label>
+        <input
+          id="chainId"
+          v-model="networkName"
+          required
+          type="text"
+          class="text-base sm:text-sm bg-gradient w-full p-2 sm:p-1 rounded-lg border-none outline-none"
+          placeholder="e.g. 40404"
+        />
+      </div>
+      <div class="space-y-1">
+        <label class="text-xs text-zinc-400" for="explorerUrl">
+          Explorer URL (Optional)
+        </label>
+        <input
+          id="explorerUrl"
+          v-model="networkName"
+          required
+          type="text"
+          class="text-base sm:text-sm bg-gradient w-full p-2 sm:p-1 rounded-lg border-none outline-none"
+          placeholder="e.g. https://explorer.dev.arcana.network/"
+        />
+      </div>
+      <div class="flex justify-center">
+        <button
+          class="text-sm sm:text-xs rounded-xl text-white dark:bg-white bg-black dark:text-black w-36 h-9 sm:w-20 sm:h-8"
+        >
+          Save
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
