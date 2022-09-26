d3487f1e0517e982457dddf25a5138532ca7b778
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-09-26 09:10:21 +0530

    Create activities store

---
+++ src/store/activities.ts
@@ -0,0 +1,67 @@
+import { defineStore } from 'pinia'
+
+type ChainId = number
+
+type TransactionOps =
+  | 'Send'
+  | 'Receive'
+  | 'Contract Deployment'
+  | 'Contract Interaction'
+
+type FileOps =
+  | 'Upload'
+  | 'Download'
+  | 'Share'
+  | 'Revoke'
+  | 'Transfer Ownership'
+  | 'Delete'
+
+type Activity = {
+  transaction: {
+    hash: string
+    operation: TransactionOps | FileOps
+    date: string
+    status: 'Success' | 'Unapproved' | 'Pending'
+    amount: number
+    nonce: number
+    gasLimit: number
+    gasUsed: number
+    gasPrice: number
+    total: number
+  }
+  address: {
+    from: string
+    to: string
+  }
+  file?: {
+    did: string
+  }
+}
+
+type ActivitiesState = {
+  activitiesByChainId: {
+    [key: ChainId]: Activity[]
+  }
+}
+
+export const useActivitiesStore = defineStore('activitiesStore', {
+  state: (): ActivitiesState => ({
+    activitiesByChainId: {},
+  }),
+  getters: {
+    activities: (state: ActivitiesState) => {
+      return (chainId) => state.activitiesByChainId[chainId]
+    },
+  },
+  actions: {
+    saveActivity(chainId: ChainId, activity: Activity) {
+      if (this.activitiesByChainId[chainId]) {
+        this.activitiesByChainId[chainId].push(activity)
+      } else {
+        this.activitiesByChainId[chainId] = [activity]
+      }
+    },
+  },
+})
+
+export type { ChainId, Activity, TransactionOps, FileOps }
