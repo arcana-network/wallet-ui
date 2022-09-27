d352116a97853de348a6b6db1af7a8b8151c1ab3
Karthik Durai <iamkarthiksiddarth@gmail.com>
2022-09-27 15:07:09 +0530

    [AR-4395] Implement UI to show pending requests (#102)
    * add icons to assets
    
    * set routes names
    
    * impl new footer design
    
    * impl logic to get theme from params
    
    * fix dot
    
    * remove toString method
    
    * set theme from appstore as default
    
    * replace with fixed logo

--- src/utils/useImage.ts
+++ src/utils/useImage.ts
@@ -1,3 +1,5 @@
+import { Theme } from '@arcana/auth'
+
 import { useAppStore } from '@/store/app'
 
 const THEME_NEUTRAL_IMAGES = [
@@ -16,11 +18,14 @@ const THEME_NEUTRAL_IMAGES = [
 export function useImage() {
   const appStore = useAppStore()
 
-  return function getImage(imageName: string): string {
+  return function getImage(
+    imageName: string,
+    theme: Theme = appStore.theme
+  ): string {
     if (THEME_NEUTRAL_IMAGES.includes(imageName)) {
       return require(`@/assets/images/${imageName}.png`)
     } else {
-      return require(`@/assets/images/${imageName}-${appStore.theme}-mode.png`)
+      return require(`@/assets/images/${imageName}-${theme}-mode.png`)
     }
   }
 }
