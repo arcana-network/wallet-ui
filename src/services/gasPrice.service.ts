ceba8ca53a81cf1fed383311ec527c0f2adc0582
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-10-17 23:36:56 +0530

    Add polygon gas station and add gas station available chain ids

--- src/services/gasPrice.service.ts
+++ src/services/gasPrice.service.ts
@@ -1,14 +1,47 @@
 import axios from 'axios'
 
-const URL = 'https://ethgasstation.info/api/ethgasAPI.json'
+const EthGasStationApi = 'https://ethgasstation.info/api/ethgasAPI.json'
+const PolygonGasStationApi = 'https://gasstation-mainnet.matic.network/v2'
+const PolygonMumbaiGasStationApi = 'https://gasstation-mumbai.matic.today/v2'
 
-const service = axios.create({
-  baseURL: URL,
-})
+const POLYGON_CHAIN_IDS = [137, 80001]
+const GAS_AVAILABLE_CHAIN_IDS = [1, 5, ...POLYGON_CHAIN_IDS]
 
-async function getGasPrice() {
-  const { data } = await service.get('')
+const GasApiMapping = {
+  1: {
+    api: EthGasStationApi,
+  },
+  5: {
+    api: EthGasStationApi,
+  },
+  137: {
+    api: PolygonGasStationApi,
+  },
+  80001: {
+    api: PolygonMumbaiGasStationApi,
+  },
+}
+
+function convertPolygonResponseToEthResponse(data) {
+  return {
+    fast: data.fast.maxFee * 10,
+    safeLow: data.safeLow.maxFee * 10,
+    average: data.standard.maxFee * 10,
+    fastWait: 0.08,
+    safeLowWait: 0.5,
+    avgWait: 0.25,
+  }
+}
+
+async function getGasPrice(chainId = 1) {
+  const gasStationUrl = GasApiMapping[chainId].api
+  const { data } = await axios.get(gasStationUrl)
+
+  if (POLYGON_CHAIN_IDS.includes(chainId)) {
+    return convertPolygonResponseToEthResponse(data)
+  }
+
   return data
 }
 
-export { getGasPrice }
+export { getGasPrice, GAS_AVAILABLE_CHAIN_IDS }
