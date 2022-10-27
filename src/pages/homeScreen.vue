eb0bfcbf8a5a2b59a1d7459da4cfafd8fe560b3e
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-10-27 17:24:24 +0530

    init account handlr, keeper, set globally to file

--- src/pages/homeScreen.vue
+++ src/pages/homeScreen.vue
@@ -60,6 +60,7 @@ const loader = ref({
   message: '',
 })
 let accountHandler: AccountHandler | null = null
+let keeper: RequestHandler | null = null
 let parentConnection: Connection<ParentConnectionApi> | null = null
 const tabs = ['Assets', 'Activity']
 const selectedTab = ref('Assets')
@@ -72,6 +73,8 @@ const assets: {
 
 onMounted(async () => {
   setRpcConfigs()
+  initAccounthandler()
+  initKeeper()
   connectToParent()
   await setTheme()
   await getRpcConfig()
@@ -118,13 +121,19 @@ async function getAccountDetails() {
   await getCurrencyExchangeRate()
 }
 
+function initAccounthandler() {
+  accountHandler = new AccountHandler(userStore.privateKey)
+  accountHandler.setProvider(rpcStore.selectedRpcConfig.rpcUrls[0])
+}
+
+async function initKeeper() {
+  keeper = new RequestHandler(accountHandler)
+}
+
 async function initAccountHandler() {
   showLoader('Please wait')
   try {
     const parentConnectionInstance = await parentConnection.promise
-
-    accountHandler = new AccountHandler(userStore.privateKey)
-    accountHandler.setProvider(rpcStore.selectedRpcConfig.rpcUrls[0])
 
     if (!userStore.walletAddress) {
       const account = accountHandler.getAccount()
@@ -136,7 +145,6 @@ async function initAccountHandler() {
       setAppMode(walletType, parentConnectionInstance)
     }
 
-    const keeper = new RequestHandler(accountHandler)
     keeper.setConnection(parentConnection)
     await keeper.setRpcConfig(rpcStore.selectedRpcConfig)
 
@@ -152,7 +160,12 @@ async function initAccountHandler() {
 }
 
 function connectToParent() {
-  const sendRequest = getSendRequestFn(handleRequest, requestStore, appStore)
+  const sendRequest = getSendRequestFn(
+    handleRequest,
+    requestStore,
+    appStore,
+    keeper
+  )
   parentConnection = createParentConnection({
     isLoggedIn: () => userStore.isLoggedIn,
     sendRequest,
