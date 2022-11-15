a81d3811568a09936b46cbd001e9c1856251b29a
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-11-15 11:17:32 +0530

    AR-4998 contract interaction error in wallet (#137)
    * Add types to process.env
    
    * Replace store.subscribe with watch
    
    * Remove parent connection destroy
    
    * Rename unwatch to unwatchRequestQueue

--- src/utils/requestManagement.ts
+++ src/utils/requestManagement.ts
@@ -1,16 +1,19 @@
 // Todo: Find a better place for these functions
 import { ethErrors, serializeError } from 'eth-rpc-errors'
+import { watch } from 'vue'
 import { useToast } from 'vue-toastification'
 
 import { requirePermission } from '@/models/Connection'
 import { router } from '@/routes'
 import { store } from '@/store'
 import { useActivitiesStore } from '@/store/activities'
+import { useRequestStore } from '@/store/request'
 import { useRpcStore } from '@/store/rpc'
 
 const activitiesStore = useActivitiesStore(store)
 const rpcStore = useRpcStore(store)
 const toast = useToast()
+const reqStore = useRequestStore()
 
 function getSendRequestFn(handleRequest, requestStore, appStore, keeper) {
   return function sendRequest(request) {
@@ -18,26 +21,36 @@ function getSendRequestFn(handleRequest, requestStore, appStore, keeper) {
   }
 }
 
-async function watchRequestQueue(reqStore, keeper) {
-  reqStore.$subscribe(async (_, state) => {
-    const { processQueue, pendingRequests } = state
-    const pendingRequestCount = Object.values(pendingRequests).length
-    const connectionInstance = await keeper.connection.promise
-    try {
-      connectionInstance.sendPendingRequestCount(pendingRequestCount)
-    } catch (err) {
-      console.error({ err })
-    }
-    while (processQueue.length > 0) {
-      const request = processQueue.shift()
-      processRequest(request, keeper)
+let unwatchRequestQueue
+
+async function watchRequestQueue(keeper) {
+  if (unwatchRequestQueue) {
+    unwatchRequestQueue()
+  }
+
+  unwatchRequestQueue = watch(
+    () => reqStore,
+    async () => {
+      const { processQueue, pendingRequests } = reqStore
+      const pendingRequestCount = Object.values(pendingRequests).length
+      const connectionInstance = await keeper.connection.promise
       try {
         connectionInstance.sendPendingRequestCount(pendingRequestCount)
       } catch (err) {
         console.error({ err })
       }
-    }
-  })
+      while (processQueue.length > 0) {
+        const request = processQueue.shift()
+        if (request) processRequest(request, keeper)
+        try {
+          connectionInstance.sendPendingRequestCount(pendingRequestCount)
+        } catch (err) {
+          console.error({ err })
+        }
+      }
+    },
+    { deep: true }
+  )
 }
 
 function getEtherInvalidParamsError(msg) {
