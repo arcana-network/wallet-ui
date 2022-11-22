ae776e207c19dbddf53dc396ed038ee21b060616
makylfang <makyl@newfang.io>
2022-11-22 09:31:14 +0400

    Added VUE_APP_WALLET_DOMAIN to process env in shims-vue.d.ts

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -16,6 +16,7 @@ declare namespace NodeJS {
     VUE_APP_SENTRY_TRACING_ORIGINS: string
     VUE_APP_WALLET_RPC_URL: string
     VUE_APP_CONTRACT_EVENT_CODE: string
+    VUE_APP_WALLET_DOMAIN: string
   }
 }
 
