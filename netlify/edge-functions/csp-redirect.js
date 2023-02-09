c207123ad6903f6cce6953fa4665e289c52e2b58
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-02-09 16:37:41 +0530

    add edge functions for CSP frame-ancestors

---
+++ netlify/edge-functions/csp-redirect.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default async (_, context) => {
+  const res = await context.next()
+  const { headers } = res
+  try {
+    const redirect = new URL(process.env.VUE_APP_WALLET_AUTH_REDIRECT_URL)
+    console.log({ redirect }, 'redirect')
+    headers.set('Content-Security-Policy', `frame-ancestors ${redirect.origin}`)
+    return new Response(res.text(), { ...res, headers })
+  } catch (e) {
+    const response = JSON.stringify({ message: 'Not allowed' })
+    return new Response(response, { status: 403 })
+  }
+}
