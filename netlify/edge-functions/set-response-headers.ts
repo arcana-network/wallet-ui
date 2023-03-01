ae3506af6908a9805e8944f480fc2876e9e65557
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-03-01 13:25:33 +0530

    replace with req header

--- netlify/edge-functions/set-response-headers.ts
+++ netlify/edge-functions/set-response-headers.ts
@@ -2,11 +2,5 @@
 import type { Context } from 'https://edge.netlify.com'
 
 export default async (request: Request, context: Context) => {
-  const url = new URL(request.url)
-
-  console.log(`Adding a custom header to the response for ${url}`)
-
-  const response = await context.next()
-  response.headers.set('X-Your-Custom-Header', 'Your custom header value')
-  return response
+  request.headers.set('X-Your-Custom-Header', 'Your custom header value')
 }
