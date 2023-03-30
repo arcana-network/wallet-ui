c6540943515f2d563267927e965ad6f3d95400ac
makylfang <makyl@newfang.io>
2023-03-30 10:53:05 +0400

    moved assets to /assets folder, added immutable caching for assets, added flutter as a login source

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 const webpack = require('webpack')
 
 module.exports = defineConfig({
+  assetsDir: 'assets',
   transpileDependencies: true,
   configureWebpack: {
     resolve: {
