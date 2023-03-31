9b1c2f2d23461b6e988610e23021ab4de5de9b98
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-03-31 15:17:47 +0530

    create wallet header

---
+++ src/components/WalletHeader.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { useAppStore } from '@/store/app'
+import { useImage } from '@/utils/useImage'
+
+const appStore = useAppStore()
+const getImage = useImage()
+
+function shrinkWallet() {
+  appStore.showWallet = false
+}
+</script>
+
+<template>
+  <header class="flex justify-between rounded-t-xl">
+    <div class="flex space-x-1 items-center">
+      <img :src="appStore.appLogo.horizontal" alt="app logo" />
+      <p>{{ appStore.name }}</p>
+    </div>
+    <button class="h-auto" @click="shrinkWallet">
+      <img :src="getImage('arrow-icon')" alt="arrow" />
+    </button>
+  </header>
+</template>
+
+<style scoped>
+header {
+  padding: var(--p-300);
+  color: var(--fg-color);
+  background: var(--container-bg-color);
+}
+</style>
