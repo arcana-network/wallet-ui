84c314c80cca589f1fed4141d1afaae593926745
makylfang <makyl@newfang.io>
2023-04-21 14:20:23 +0400

    Added unity check in redirect and mfa

--- src/pages/loginRedirect.vue
+++ src/pages/loginRedirect.vue
@@ -81,7 +81,11 @@ async function init() {
           }
         )
       } else {
-        if (loginSrc === 'rn' || loginSrc === 'flutter') {
+        if (
+          loginSrc === 'rn' ||
+          loginSrc === 'flutter' ||
+          loginSrc === 'unity'
+        ) {
           await connectionToParent.goToWallet()
           return
         }
