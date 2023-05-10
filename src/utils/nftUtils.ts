bb709aec6418c10369dfb518fda609f9d33d91ca
shrinathprabhu <shrinathprabhu.123@gmail.com>
2023-05-10 12:04:08 +0530

    Ignore NFTs that error out during fetching details

--- src/utils/nftUtils.ts
+++ src/utils/nftUtils.ts
@@ -164,7 +164,7 @@ async function getDetailedNFTs(nftDB: NFTDB, chainId: number) {
             }),
         getNFTDetails(nft.tokenUrl, nft.tokenId),
       ])
-      if (ownership != undefined && !ownership.owner) {
+      if ((ownership != undefined && !ownership.owner) || !details) {
         nftDB.removeNFT(nft, chainId)
         return
       }
