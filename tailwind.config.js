456fc66b29bae4bce345c0f9e0345026acb89934
shrinathprabhu <shrinathprabhu.123@gmail.com>
2023-05-10 15:55:17 +0530

    Add base button component

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,53 +3,69 @@ module.exports = {
   content: ['./src/**/*.{vue,js,ts,jsx,tsx,css}'],
   darkMode: 'class',
   theme: {
-    fontSize: {
-      xs: '10px',
-      sm: '12px',
-      base: '14px',
-      lg: '16px',
-      xl: '18px',
-      xxl: '28px',
-    },
-    fontWeight: {
-      light: 300,
-      normal: 400,
-      medium: 500,
-      bold: 700,
-    },
-    colors: {
-      red: '#ff4264',
-      yellow: '#eeb113',
-      blue: '#3e9aff',
-      green: '#05c168',
-      black: {
-        100: '#000000',
+    extend: {
+      fontSize: {
+        xs: '10px',
+        sm: '12px',
+        base: '14px',
+        lg: '16px',
+        xl: '18px',
+        xxl: '28px',
       },
-      white: {
-        100: '#ffffff',
-        200: '#f7f7f7',
+      fontWeight: {
+        light: 300,
+        normal: 400,
+        medium: 500,
+        bold: 700,
       },
-      gray: {
-        100: '#050505',
-        200: '#141414',
-        300: '#1f1f1f',
-        400: '#8d8d8d',
-        500: '#363636',
-        600: '#313131',
-        700: '#050505',
-        800: '#dcdcdc',
-        900: '#2c2c2c',
+      colors: {
+        red: { 100: '#ff4264' },
+        yellow: { 100: '#eeb113' },
+        blue: { 100: '#3e9aff' },
+        green: { 100: '#05c168' },
+        black: {
+          100: '#000000',
+          200: '#1f1f1f',
+          300: '#141414',
+          400: '#050505',
+        },
+        white: {
+          100: '#ffffff',
+          200: '#f7f7f7',
+        },
+        gray: {
+          100: '#8d8d8d',
+          200: '#363636',
+          300: '#313131',
+          400: '#dcdcdc',
+          500: '#2c2c2c',
+        },
       },
-    },
-    width: {
-      icon: {
-        xs: '14px',
-        sm: '16px',
-        md: '18px',
-        lg: '20px',
-        xl: '24px',
-        xxl: '32px',
-        xxxl: '34px',
+      width: {
+        icon: {
+          xs: '14px',
+          sm: '16px',
+          md: '18px',
+          lg: '20px',
+          xl: '24px',
+          xxl: '32px',
+          xxxl: '34px',
+        },
+      },
+      borderWidth: {
+        1: '1px',
+        default: '1.5px',
+      },
+      borderRadius: {
+        sm: '5px',
+      },
+      boxShadow: {
+        sm: '0 0 8px 3px rgba(0, 0, 0, 0.05)',
+      },
+      boxShadowColor: {
+        dark: {
+          100: 'rgba(126, 126, 126, 0.25)',
+        },
       },
     },
   },
