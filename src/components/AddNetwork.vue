8d5487d821c66a6a5cd8d4a33ba77f33e366462d
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-05-16 14:32:34 +0530

    if RPC URL is not valid, show a error toast

--- src/components/AddNetwork.vue
+++ src/components/AddNetwork.vue
@@ -37,44 +37,48 @@ function isExistingChain(chainId) {
 }
 
 async function handleSubmit() {
-  const rpcUrl = rpcConfig.value.rpcUrl
-  const chainId = rpcConfig.value.chainId
-  const existingChain = isExistingChain(chainId)
-  if (isExistingRpcUrl(rpcUrl)) {
-    return toast.error(
-      `RPC URL - ${rpcUrl} already exists, please use different one`
-    )
-  } else {
-    const provider = new ethers.providers.StaticJsonRpcProvider(
-      rpcConfig.value.rpcUrl
-    )
-    const chainId = await provider.getNetwork()
-    if (Number(chainId.chainId) !== Number(rpcConfig.value.chainId)) {
-      return toast(`Incorrect combination of chainId and rpcUrl`)
-    }
-    if (existingChain) {
-      rpcStore.setRpcConfig({
-        ...existingChain,
-        rpcUrls: [rpcConfig.value.rpcUrl],
-      })
-      rpcStore.setSelectedChainId(existingChain.chainId)
+  try {
+    const rpcUrl = rpcConfig.value.rpcUrl
+    const chainId = rpcConfig.value.chainId
+    const existingChain = isExistingChain(chainId)
+    if (isExistingRpcUrl(rpcUrl)) {
+      return toast.error(
+        `RPC URL - ${rpcUrl} already exists, please use different one`
+      )
     } else {
-      const payload = {
-        chainName: rpcConfig.value.networkName,
-        chainId: rpcConfig.value.chainId,
-        blockExplorerUrls: [rpcConfig.value.explorerUrl],
-        rpcUrls: [rpcConfig.value.rpcUrl],
-        favicon: 'blockchain-icon',
-        nativeCurrency: {
-          symbol: rpcConfig.value.currencySymbol,
-          decimals: 18,
-        },
-        isCustom: true,
+      const provider = new ethers.providers.StaticJsonRpcProvider(
+        rpcConfig.value.rpcUrl
+      )
+      const chainId = await provider.getNetwork()
+      if (Number(chainId.chainId) !== Number(rpcConfig.value.chainId)) {
+        return toast(`Incorrect combination of chainId and rpcUrl`)
       }
-      rpcStore.addNetwork(payload)
-      rpcStore.setSelectedChainId(payload.chainId)
+      if (existingChain) {
+        rpcStore.setRpcConfig({
+          ...existingChain,
+          rpcUrls: [rpcConfig.value.rpcUrl],
+        })
+        rpcStore.setSelectedChainId(existingChain.chainId)
+      } else {
+        const payload = {
+          chainName: rpcConfig.value.networkName,
+          chainId: rpcConfig.value.chainId,
+          blockExplorerUrls: [rpcConfig.value.explorerUrl],
+          rpcUrls: [rpcConfig.value.rpcUrl],
+          favicon: 'blockchain-icon',
+          nativeCurrency: {
+            symbol: rpcConfig.value.currencySymbol,
+            decimals: 18,
+          },
+          isCustom: true,
+        }
+        rpcStore.addNetwork(payload)
+        rpcStore.setSelectedChainId(payload.chainId)
+      }
+      emit('close')
     }
-    emit('close')
+  } catch (e) {
+    toast.error('Invalid RPC URL')
   }
 }
 </script>
