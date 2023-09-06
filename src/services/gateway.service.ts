6371ee1e4461b3a3687fe09da87d88e3de5ef41c
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-09-06 13:22:40 +0530

    replace API to check gastank config status

--- src/services/gateway.service.ts
+++ src/services/gateway.service.ts
@@ -4,10 +4,10 @@ import { API } from '@/utils/constants'
 
 const gatewayInstance = axios.create({ baseURL: API.gateway })
 
-function getGastankAPIKey(appId, chainId) {
-  return gatewayInstance.get('/api/v1/gastank/api-key/', {
-    params: { app_address: appId, chain_id: chainId },
+function checkIfGastankConfigured(appId, chainId) {
+  return gatewayInstance.get('/api/v1/gastank/status/', {
+    params: { app_id: appId, chain_id: chainId },
   })
 }
 
-export { getGastankAPIKey }
+export { checkIfGastankConfigured }
