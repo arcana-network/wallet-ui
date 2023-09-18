6c3f7b35689a59fd537cfe09999fb62ef93f0ad2
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-09-18 15:45:18 +0530

    comment out to test

--- src/pages/MFASetup.vue
+++ src/pages/MFASetup.vue
@@ -253,11 +253,11 @@ async function handlePinProceed() {
         return toast.error(e as string)
       }
       console.log({ e }, 'handlePinProceed-error')
-      return connectionToParent.error(
-        e as string,
-        // eslint-disable-next-line no-undef
-        process.env.VUE_APP_WALLET_DOMAIN
-      )
+      // return connectionToParent.error(
+      //   e as string,
+      //   // eslint-disable-next-line no-undef
+      //   process.env.VUE_APP_WALLET_DOMAIN
+      // )
     }
     loader.value = {
       show: false,
