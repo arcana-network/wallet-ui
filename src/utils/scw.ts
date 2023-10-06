39c1c9c5857ed0e368b64abf08e09e2b4400f5a8
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-10-06 17:46:03 +0530

    pass gateway URL init fn

--- src/utils/scw.ts
+++ src/utils/scw.ts
@@ -1,11 +1,13 @@
 import SCW from '@arcana/scw'
 
+const gatewayUrl = process.env.VUE_APP_WALLET_GATEWAY
+
 const scwInstance = new SCW()
 
 async function initSCW(appId: string, provider: any) {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  await scwInstance.init(appId, provider)
+  await scwInstance.init(appId, provider, gatewayUrl)
 }
 
 export { initSCW, scwInstance }
