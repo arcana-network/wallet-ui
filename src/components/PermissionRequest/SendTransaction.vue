ad85bc4da62f6e2b683680fa62ece1a5d23386a1
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-10-27 16:46:52 +0530

    replace with request var

--- src/components/PermissionRequest/SendTransaction.vue
+++ src/components/PermissionRequest/SendTransaction.vue
@@ -5,7 +5,7 @@ import SignMessageAdvancedInfo from '@/components/signMessageAdvancedInfo.vue'
 import { truncateMid } from '@/utils/stringUtils'
 
 const props = defineProps({
-  data: {
+  request: {
     type: Object,
     required: true,
   },
@@ -41,23 +41,25 @@ function calculateGasPrice(gasPrice) {
     class="flex-1 flex flex-col bg-[#141414] p-4 h-full rounded-md space-y-4 border-2 overflow-auto"
   >
     <p class="text-sm font-medium">Transaction Details</p>
-    <div v-if="data.params[0]?.to" class="flex justify-between">
+    <div v-if="request.params[0]?.to" class="flex justify-between">
       <span class="text-sm">To</span>
-      <span class="text-sm">{{ truncateMid(data.params[0]?.to) }}</span>
+      <span class="text-sm">{{ truncateMid(request.params[0]?.to) }}</span>
     </div>
     <div class="flex justify-between">
       <span class="text-sm">Gas Price</span>
       <span class="text-sm">{{
-        calculateGasPrice(data.params[0].gas || data.params[0].gasPrice)
+        calculateGasPrice(request.params[0].gas || request.params[0].gasPrice)
       }}</span>
     </div>
-    <div v-if="data.params[0]?.value" class="flex justify-between">
+    <div v-if="request.params[0]?.value" class="flex justify-between">
       <span class="text-sm">Value</span>
-      <span class="text-sm">{{ calculateValue(data.params[0]?.value) }}</span>
+      <span class="text-sm">{{
+        calculateValue(request.params[0]?.value)
+      }}</span>
     </div>
-    <div v-if="data.params[0]?.data" class="flex flex-col space-y-2 flex-1">
+    <div v-if="request.params[0]?.data" class="flex flex-col space-y-2 flex-1">
       <span class="text-sm">Data</span>
-      <SignMessageAdvancedInfo :info="data.params[0]?.data" />
+      <SignMessageAdvancedInfo :info="request.params[0]?.data" />
     </div>
   </div>
 </template>
