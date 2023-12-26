ee5d284537909d8e5264b6fc543b14e6948f943a
makylfang <makyl@newfang.io>
2023-12-26 15:50:55 +0400

    Fixing mfa restore for global

--- src/pages/MFARestoreScreen.vue
+++ src/pages/MFARestoreScreen.vue
@@ -14,14 +14,17 @@ import { useToast } from 'vue-toastification'
 import AppLoader from '@/components/AppLoader.vue'
 import PinBasedRecoveryModal from '@/components/PinBasedRecoveryModal.vue'
 import SecurityQuestionRecoveryModal from '@/components/SecurityQuestionRecoveryModal.vue'
-import type { RedirectParentConnectionApi } from '@/models/Connection'
+import type {
+  GlobalRedirectMethods,
+  RedirectParentConnectionApi,
+} from '@/models/Connection'
 import { getAppConfig } from '@/services/gateway.service'
 import { useAppStore } from '@/store/app'
 import { useUserStore } from '@/store/user'
 import { GATEWAY_URL, AUTH_NETWORK, SESSION_EXPIRY_MS } from '@/utils/constants'
 import { devLogger } from '@/utils/devLogger'
 import { getLoginToken } from '@/utils/loginToken'
-import { handleLogin } from '@/utils/redirectUtils'
+import { handleGlobalLogin, handleLogin } from '@/utils/redirectUtils'
 import { getStorage, initStorage } from '@/utils/storageWrapper'
 
 const user = useUserStore()
@@ -212,16 +215,12 @@ async function handlePinBasedRecovery(ev: any) {
 }
 
 async function returnToParent(key: string) {
-  const connectionToParent = await connectToParent<RedirectParentConnectionApi>(
-    {}
-  ).promise
   const info = storage.session.getUserInfo()
   if (!info) {
     return
   }
   const loginSrc = storage.local.getLoginSrc()
   const state = storage.session.getState() as string
-
   const stateInfo = global ? { i: state } : decodeJSON<StateInfo>(state)
   const isStandalone =
     loginSrc === 'rn' || loginSrc === 'flutter' || loginSrc === 'unity'
@@ -259,6 +258,18 @@ async function returnToParent(key: string) {
     timestamp: Date.now(),
   })
   const messageId = getUniqueId()
+  if (global) {
+    await handleGlobalLogin({
+      connection: await connectToParent<GlobalRedirectMethods>({}).promise,
+      userInfo: info,
+      state: stateInfo.i,
+      sessionID: uuid,
+      sessionExpiry: Date.now() + SESSION_EXPIRY_MS,
+      messageId,
+      isStandalone,
+    })
+    return
+  }
   await handleLogin({
     state: stateInfo.i,
     isStandalone,
@@ -266,7 +277,7 @@ async function returnToParent(key: string) {
     sessionID: uuid,
     sessionExpiry: Date.now() + SESSION_EXPIRY_MS,
     messageId,
-    connection: connectionToParent,
+    connection: await connectToParent<RedirectParentConnectionApi>({}).promise,
   })
     .catch(async (e) => {
       if (e instanceof Error) {
