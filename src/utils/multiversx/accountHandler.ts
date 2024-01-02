4f9caf7aeb1b31f3b97d083e1e566158a38df0e0
mmjee <root@git.maharshi.ninja>
2024-01-02 14:42:47 +0530

    Basic account handler

---
+++ src/utils/multiversx/accountHandler.ts
@@ -0,0 +1,58 @@
+import {
+  SignableMessage,
+  type ISignature,
+  Transaction,
+} from '@multiversx/sdk-core'
+import {
+  ApiNetworkProvider,
+  AccountOnNetwork,
+} from '@multiversx/sdk-network-providers'
+import type { TransactionOnNetwork } from '@multiversx/sdk-network-providers/out'
+import { UserSecretKey, UserPublicKey } from '@multiversx/sdk-wallet'
+import { Signature } from '@multiversx/sdk-wallet/out/signature'
+
+export class MultiversXAccountHandler {
+  private privateKey: UserSecretKey
+  private publicKey: UserPublicKey
+  private readonly addrStr: string
+  private conn: ApiNetworkProvider
+
+  constructor(privateKey: Uint8Array | Buffer, rpcURL: string) {
+    this.privateKey = new UserSecretKey(privateKey)
+    this.publicKey = this.privateKey.generatePublicKey()
+    this.addrStr = this.publicKey.toAddress().toString()
+    this.conn = new ApiNetworkProvider(rpcURL)
+  }
+
+  async getAccountOnNetwork(): Promise<AccountOnNetwork> {
+    return this.conn.getAccount(this.publicKey.toAddress())
+  }
+
+  // the accounts are returned in serialized form
+  getAccounts(): string[] {
+    return [this.addrStr]
+  }
+
+  async setProvider(rpcURL) {
+    this.conn = new ApiNetworkProvider(rpcURL)
+  }
+
+  signTransactions(txes: Transaction[]): Uint8Array[] {
+    return txes.map((tx) => this.privateKey.sign(tx.serializeForSigning()))
+  }
+
+  signMessage(signableMessage: SignableMessage): ISignature {
+    return new Signature(
+      this.privateKey.sign(signableMessage.serializeForSigning())
+    )
+  }
+
+  getTransaction(hash: string): Promise<TransactionOnNetwork> {
+    return this.conn.getTransaction(hash)
+  }
+
+  async getLatestBlockHash() {
+    const s = await this.conn.getNetworkStatus()
+    return s.HighestFinalNonce
+  }
+}
