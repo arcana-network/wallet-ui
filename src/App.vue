babb081dd97e3e3a107a63519a59b8eeb4421726
karthikdurai <iamkarthiksiddarth@gmail.com>
2024-01-18 17:33:46 +0530

    remove v-if for now

--- src/App.vue
+++ src/App.vue
@@ -165,7 +165,7 @@ const showHeader = computed(() => {
           :src="getImage('secured-by-arcana.svg')"
           class="h-3 select-none mt-5"
         />
-        <BaseModal v-if="modal.show" />
+        <BaseModal />
       </div>
       <WalletFooter v-if="showFooter && !starterTipsStore.show" />
     </div>
