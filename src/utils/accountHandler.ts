9d7aa321a8c092bb85f7eca623128629cc5b1a48
karthikdurai <iamkarthiksiddarth@gmail.com>
2024-01-29 11:01:13 +0530

    add multiversex handler

--- src/utils/accountHandler.ts
+++ src/utils/accountHandler.ts
@@ -2,9 +2,13 @@ import base58 from 'bs58'
 
 import { ChainType } from '@/utils/chainType'
 import { EVMAccountHandler } from '@/utils/evm/accountHandler'
+import { MultiversXAccountHandler } from '@/utils/multiversx/accountHandler'
 import { SolanaAccountHandler } from '@/utils/solana/accountHandler'
 
-type AccountHandler = EVMAccountHandler | SolanaAccountHandler
+type AccountHandler =
+  | EVMAccountHandler
+  | SolanaAccountHandler
+  | MultiversXAccountHandler
 
 function CreateAccountHandler(
   privateKey: string,
@@ -12,7 +16,8 @@ function CreateAccountHandler(
   chainType: ChainType = ChainType.evm_secp256k1
 ): AccountHandler {
   if (chainType === ChainType.solana_cv25519) {
-    return new SolanaAccountHandler(base58.decode(privateKey), rpcUrl)
+    // return new SolanaAccountHandler(base58.decode(privateKey), rpcUrl)
+    return new MultiversXAccountHandler(base58.decode(privateKey), rpcUrl)
   }
   return new EVMAccountHandler(privateKey, rpcUrl)
 }
@@ -21,6 +26,7 @@ export {
   CreateAccountHandler,
   type EVMAccountHandler,
   type SolanaAccountHandler,
+  type MultiversXAccountHandler,
   type AccountHandler,
   type ChainType as SupportedChainType,
 }
