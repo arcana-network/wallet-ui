8bfb056777d7b39e6217a68245f3fbebf69cce41
Karthik Durai <iamkarthiksiddarth@gmail.com>
2024-02-23 11:22:00 +0530

    AR-7536 - Testnet (#605)
    * [AR-7536] wallet starter tips bug on main-net (#584)
    
    * fix dot issue
    
    * add skip btn
    
    * remove div
    
    * fix starter tips for light theme
    
    ---------
    
    Co-authored-by: Shrinath Prabhu <shrinathprabhu.123@gmail.com>

--- src/pages/StarterTips/page-6.vue
+++ src/pages/StarterTips/page-6.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
-const dots = [1, 2, 3, 4]
+const dots = [1, 2, 3, 4, 5, 6]
 </script>
 
 <template>
   <div class="h-full w-full flex flex-col">
-    <div class="h-3/4 w-full bg-black-100 bg-opacity-80">
+    <div class="h-3/4 w-full">
       <div class="h-full w-full startertips">
         <div
           class="absolute z-[2147483648] w-36 h-36 top-[266px] right-8"
         ></div>
       </div>
     </div>
-    <div class="bg-black-100 h-1/4 p-4">
+    <div
+      class="text-black-100 dark:text-white-100 bg-gray-900 dark:bg-black-100 h-1/4 p-4"
+    >
       <h1 class="text-xl uppercase font-black">On-RAMP Support</h1>
       <p class="text-sm">
         Easily purchase a wide range of crypto assets directly within the wallet
@@ -22,7 +24,7 @@ const dots = [1, 2, 3, 4]
           v-for="dot in dots"
           :key="dot"
           class="block h-[5px] w-[5px] rounded-full"
-          :class="dot === 4 ? 'bg-[#FFFFFF]' : 'bg-[#8D8D8D]'"
+          :class="dot === 6 ? 'dark:bg-white-100 bg-black-100' : 'bg-[#8D8D8D]'"
         ></span>
       </div>
     </div>
