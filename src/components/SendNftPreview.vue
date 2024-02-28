300ee6c3e8f556ccaa96b8ede890f8775ef2067c
Muktanshu Mishra <muktanshumishra24@gmail.com>
2024-02-28 15:00:41 +0530

    Design Feedbacks Implemented for Gasless Transactions (#610)
    * Design Feedbacks Implemented
    
    * Review Changes Implemented
    
    * Review Implemented

--- src/components/SendNftPreview.vue
+++ src/components/SendNftPreview.vue
@@ -114,12 +114,7 @@ function truncateAddress(address: string) {
         </div>
       </div>
       <span
-        v-if="rpcStore.useGasless && paymasterBalance < 0.1"
-        class="text-xs text-red-100 font-medium text-center w-full"
-        >Gasless Transaction not available.
-      </span>
-      <span
-        v-else-if="rpcStore.useGasless && paymasterBalance > 0.1"
+        v-if="rpcStore.useGasless && paymasterBalance >= 0.1"
         class="text-xs text-green-100 font-medium text-center w-full"
         >This is a Gasless Transaction. Click Below to Approve.
       </span>
--- src/pages/SendTokens.vue
+++ src/pages/SendTokens.vue
@@ -670,11 +670,6 @@ watch(
           @gas-price-input="handleSetGasPrice"
         />
         <span
-          v-if="rpcStore.useGasless && paymasterBalance < 0.1"
-          class="text-xs text-red-100 font-medium text-center w-full"
-          >Gasless Transaction not available.
-        </span>
-        <span
           v-else-if="rpcStore.useGasless && paymasterBalance >= 0.1"
           class="text-xs text-green-100 font-medium text-center w-full"
           >This is a Gasless Transaction. Click Below to Approve.
