0a63974bcd701efc286d6c9c492c1ee043c90fda
karthikdurai <iamkarthiksiddarth@gmail.com>
2024-05-21 12:21:15 +0530

    debug

--- src/components/SendTokensPreview.vue
+++ src/components/SendTokensPreview.vue
@@ -119,7 +119,7 @@ function truncateAddress(address: string) {
             "
             class="text-base text-green-100"
           >
-            Sponsored - {{ transactionMode }}
+            Sponsored - {{ transactionMode }} - {{ paymasterBalance }}
           </span>
           <span
             v-else-if="!loader.show && transactionMode.length === 0"
