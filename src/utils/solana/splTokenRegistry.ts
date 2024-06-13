0c06fdd076c75e2de308186c29c8be7eb16658bf
shrinathprabhu <shrinathprabhu.123@gmail.com>
2024-06-13 13:13:04 +0530

    Replace token registry with fetching tokens from cdn

--- src/utils/solana/splTokenRegistry.ts
+++ src/utils/solana/splTokenRegistry.ts
@@ -1,5 +1,35 @@
-import { TokenInfo, TokenListProvider } from '@solana/spl-token-registry'
+import axios from 'axios'
 
+const TOKEN_REGISTRY_URL =
+  'https://cdn.jsdelivr.net/gh/solana-labs/token-list@main/src/tokens/solana.tokenlist.json'
+
+const TOKEN_REGISTRY_URL_FALLBACK =
+  'https://raw.githubusercontent.com/solana-labs/token-list/main/src/tokens/solana.tokenlist.json'
+
+type TokenInfo = {
+  chainId: 101 | 102 | 103
+  address: string
+  symbol: string
+  name: string
+  decimals: number
+  logoURI: string
+  tags: string[]
+}
+
+async function fetchTokens() {
+  const cdnRegistryFetch = axios.get(TOKEN_REGISTRY_URL)
+  const fallbackRegistryFetch = axios.get(TOKEN_REGISTRY_URL_FALLBACK)
+  try {
+    const tokens = await Promise.race([cdnRegistryFetch, fallbackRegistryFetch])
+    if (tokens?.data?.tokens?.length === 0) {
+      throw new Error('Token list is empty')
+    }
+    return tokens.data.tokens
+  } catch (e) {
+    console.error('Could not fetch token list', e)
+    return []
+  }
+}
 class SPLTokenRegistry {
   private splTokens: Map<string, TokenInfo>
 
@@ -8,7 +38,7 @@ class SPLTokenRegistry {
   }
 
   static async create() {
-    const tokens = await new TokenListProvider().resolve()
+    const tokens = await fetchTokens()
     const map = new Map<string, TokenInfo>()
     tokens.getList().forEach((item) => {
       map.set(item.address, item)
